961bf1f60d5cc16a74b641cb4a702e45af712677
Arthur Kalimullin <kalimullin@gmail.com>
2013-02-21 19:24:28 +0600

    Ray tracer singleton

--- src/main/java/org/kalimullin/fsraytracer/Kernel.java
+++ src/main/java/org/kalimullin/fsraytracer/Kernel.java
@@ -1,11 +1,21 @@
 package org.kalimullin.fsraytracer;
 
+import org.kalimullin.fsraytracer.data.SceneDataProvider;
+import org.kalimullin.fsraytracer.scene.Scene;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
 
 public class Kernel {
     private static Logger logger = LoggerFactory.getLogger(Kernel.class);
     public static void main(String[] args) {
-        logger.info("Kernel");
+        File sceneFile = new File(args[0]);
+        Scene scene = new Scene();
+        scene.setSceneObjects(new SceneDataProvider(sceneFile).getSceneObjects());
+        RayTracer.getInstance().setScene(scene);
+        RayTracer.getInstance().startListening(new BufferedReader(new InputStreamReader(System.in)));
     }
 }
